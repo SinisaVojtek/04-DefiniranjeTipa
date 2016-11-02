11f05c160458c13f4f1f010b0dfa66b97936412a
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-11-02 18:27:20 +0100

    Riješen ISisposable,,,

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -3,18 +3,15 @@ using System.IO;
 
 namespace Vsite.CSharp
 {
-    public class Disposable
+    public class Disposable 
     {
         public static void StvaranjeIPisanjeUDatoteku(string imeDatoteke)
         {
             StreamWriter sw = new StreamWriter(imeDatoteke);
             sw.WriteLine("Ovo je moj upis");
-            // TODO: Pokrenuti program i pogledati ispis.
 
-            // TODO: Dodati poziv metode StreamWriter.Dispose (ili StreamWriter.Close) te ponovno pokrenuti program.
-
-            // TODO: Preraditi kod metode tako da se umjesto metode Dispose koristi blok using
-
+            sw.Dispose();
+           
         }
 
         public static void BrisanjeDatoteke(string imeDatoteke)
