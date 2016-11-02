001d55500377b916f3f374c983d803a687cc5a85
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-11-02 18:08:43 +0100

    Rješeno forsirano oslobađanje memorije

--- GCCollect/GCCollect.cs
+++ GCCollect/GCCollect.cs
@@ -5,12 +5,28 @@ namespace Vsite.CSharp
 {
     class Program
     {
-        // TODO: Prekopirati definiciju klase s destruktorom iz prethodnog primjera
-        // TODO: Staviti točku prekida (breakpoint) u destruktor, pokrenuti program i pogledati zadnje ispise prije prekida izvođenja.
         public class KlasaSDestruktorom
         {
+            public KlasaSDestruktorom()
+            {
+                RedniBroj = ++Brojač;
+                string poruka = string.Format("Konstruktor objekta br. {0}", RedniBroj);
+                // ispis u konzolu
+                Console.WriteLine(poruka);
+                // dijagnostički ispis u Output prozor Visual Studija
+                Debug.WriteLine(poruka);
+            }
 
+            ~KlasaSDestruktorom() {
+                Console.WriteLine("Destruktor objekta br. {0}.", RedniBroj);
+                Debug.WriteLine("Destruktor objekta br. {0}.", RedniBroj);
+            }
+
+            public readonly int RedniBroj; // redni broj objekta
+
+            static int Brojač = 0; // brojač ukupno stvorenih objekata
         }
+       
 
         static void Main(string[] args)
         {
